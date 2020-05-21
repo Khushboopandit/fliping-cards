544501396d592f1e0d296c81e92305d6af46baad
Khushboo-ms <56860877+Khushboo-ms@users.noreply.github.com>
2020-05-21 12:38:35 +0530

    Updates

---
+++ precache-manifest.8c2ce735c68799b63e353b85840e7310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeac1f34106b0543186830ae731b2a31",
+    "url": "/fliping-cards/index.html"
+  },
+  {
+    "revision": "da3dba274e89a614a837",
+    "url": "/fliping-cards/static/css/main.cf39ec6f.chunk.css"
+  },
+  {
+    "revision": "d1613a82620685ea36ce",
+    "url": "/fliping-cards/static/js/2.3c5894cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fliping-cards/static/js/2.3c5894cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3dba274e89a614a837",
+    "url": "/fliping-cards/static/js/main.8fbdc0bb.chunk.js"
+  },
+  {
+    "revision": "90ccfc16260e1eb6727d",
+    "url": "/fliping-cards/static/js/runtime-main.ebd4a1ca.js"
+  }
+]);
